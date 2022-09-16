674add2bf5080055059fe426f027851b215252b3
79277 <79277@LAPTOP-4JH0IHNA>
2022-09-16 15:53:10 +0300

    Добавьте файлы проекта.

---
+++ math3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ceilin: " + Math.Ceiling(a));
+
+        Console.WriteLine("Floor  " + Math.Floor(a));
+        Console.WriteLine("Truncate:  " + Math.Truncate(a));
+        Console.ReadKey();
+    }
+}
